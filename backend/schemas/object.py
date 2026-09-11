ccbaa8953106dd8099af586c425a9799c3b3bfc7
sahautsab13-lgtm <sahautsab13@gmail.com>
2026-09-12 01:31:11 +0530

    Add object API schema

---
+++ backend/schemas/object.py
@@ -0,0 +1,14 @@
+"""Object API schemas."""
+from pydantic import BaseModel
+
+
+class TLEData(BaseModel):
+    line1: str
+    line2: str
+
+
+class ObjectResponse(BaseModel):
+    object_id: str
+    name: str
+    tle: TLEData
+    epoch: str
