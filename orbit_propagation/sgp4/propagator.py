852c61700160389b4dbac3a6002b86c99c4bb7d6
Jithu Tharun <tharunjithu0@gmail.com>
2026-09-11 19:40:25 +0530

    Set up SGP4 propagation module

---
+++ orbit_propagation/sgp4/propagator.py
@@ -0,0 +1,17 @@
+"""SGP4 propagator foundation for TLE/GP orbit propagation.
+
+This module is a placeholder. Actual SGP4 propagation will use the
+trusted ``sgp4`` package and will be implemented in a later task.
+"""
+
+from __future__ import annotations
+
+
+class SGP4Propagator:
+    """SGP4/SDP4 propagator for two-line element (TLE) and GP data.
+
+    Not implemented yet.
+    """
+
+    def propagate(self, *args, **kwargs):
+        raise NotImplementedError("SGP4 propagation is not implemented yet")
